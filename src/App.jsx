74c8a0b88b63a4c2bb69afbd9cf0436f5f6b1cdc
Abhishek Kumar <abhikr.ak.2001@gmail.com>
2024-06-07 19:38:18 +0530

    notes local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Notes from "./Components/Notes";
+import NotesLocalStorage from "./Components/NotesLocalStorage";
 
 const App = () => {
   return (
     <div>
-      <Notes />
+      {/* <Notes /> */}
+      <NotesLocalStorage />
     </div>
   );
 };
